a8670e51bff499aa181af041aece9f6393ed40e5
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-06-25 21:09:13 +0800

    created database

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
 import Resizer
 
 config = Resizer.config
 secret = Resizer.secret
+cwd = Resizer.cwd
+
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = config("upload_folder")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{cwd}{config('sqlalchemy_database_uri')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config.bool("sqlalchemy_track_modifications")
 app.config["SECRET_KEY"] = secret("secret_key")
+
+database = SQLAlchemy(app)
+migrate = Migrate(app, database)
+
+
+class Images(database.Model):
+    __tablename__ = "Images"
+    id = database.Column(database.Integer, primary_key=True)
+    dateUploaded = database.Column(database.Date, index=True, unique=True, default=datetime.utcnow)
+    imageFileName = database.Column(database.String(128), index=True, unique=True)
+    sizeFrom = database.Column(database.String(32), index=True, unique=True)
+    sizeTo = database.Column(database.String(32))
+    resizeStatus = database.Column(database.String(8), index=True, unique=False)
+
+    def __repr__(self):
+        return '<Upload date: {}>'.format(self.dateUploaded)
 
 
 @app.route("/", methods=["GET"])
@@ -36,7 +59,3 @@ def show_status(order_id):
 @app.errorhandler(404)
 def show_404(error):
     return render_template("error.html", page=request.base_url.split("//")[-1])
-
-
-if __name__ == "__main__":
-    app.run()
