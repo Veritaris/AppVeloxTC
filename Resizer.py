e546d879f68f3d8b29f17aa46a00ae68d5c50e83
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-06-26 21:11:39 +0800

    reworked database

--- Resizer.py
+++ Resizer.py
@@ -1,9 +1,8 @@
 from flask import request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 from multiprocessing import Process
-from DatabaseModels import Images
+from DatabaseModels import Images, database
 from multiprocessing import Lock
-from Database import database
 from datetime import datetime
 from envparse import Env
 from flask import json
