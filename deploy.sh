21a173ce77d0a9284ac6ccadec77bae64bd12b7d
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-06-30 18:16:35 +0800

    Some final changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 cd AppVeloxTC
 git pull
+pip3 install -r requirements.txt
+pip3 install coveralls
+COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN coveralls
 sudo service nginx reload
 sudo service unit restart
